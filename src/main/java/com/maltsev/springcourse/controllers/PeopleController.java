9cf1b130836157d9cad4117a60c8c8372d8cc6d4
me <me>
2021-01-03 21:51:25 +0100

    lesson23

--- src/main/java/com/maltsev/springcourse/controllers/PeopleController.java
+++ src/main/java/com/maltsev/springcourse/controllers/PeopleController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.jws.WebParam;
+
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
@@ -31,18 +33,33 @@ public class PeopleController {
     }
 
     @GetMapping("/{id}/edit")
-    public String edit(Model model, @PathVariable("id") int id){
+    public String edit(Model model, @PathVariable("id") int id) {
         model.addAttribute("person", personDAO.show(id));
         return "people/edit";
     }
+
     @GetMapping("/new")
-    public String newPerson(Model model){
-        model.addAttribute("person",new Person());
+    public String newPerson(Model model) {
+        model.addAttribute("person", new Person());
         return "people/new";
     }
+
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person){
+    public String create(@ModelAttribute("person") Person person) {
         personDAO.save(person);
         return "redirect:/people";
     }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person,
+                         @PathVariable("id") int id) {
+        personDAO.update(id,person);
+        return "redirect:/people";
+    }
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id){
+        personDAO.delete(id);
+        return "redirect:/people";
+    }
+
 }
